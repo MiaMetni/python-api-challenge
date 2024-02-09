f88305369f8868663c9da03e715de84f582a23e1
mmetni-Ztydmy <mmetni@bu.edu>
2024-02-09 04:44:28 -0500

    final

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "75af1615c46466465144360fa031d7fb"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "e50634c0daec4f1aa97717fc14d8521e"
